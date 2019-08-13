94dfc3ecec1498b650c4d5b2eb1b6c6d45b06f31
NicolaAstra <mcsnow78@gmail.com>
2019-08-13 10:36:27 +0700

    setup links and icons

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/Layout"
+import { FaJoint } from "react-icons/fa"
 
 export default () => (
   <>
     <Layout>
       Hello world! <Link to="/blog/"> Blog page</Link>
+      <FaJoint />
     </Layout>
   </>
 )
