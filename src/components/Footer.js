6ab65fe3b1e668181e69cdda7973172f52b82298
NicolaAstra <mcsnow78@gmail.com>
2019-08-13 14:55:21 +0700

    styeled footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,41 @@
 import React from "react"
+import { Link } from "gatsby"
+import styles from "../css/footer.module.css"
+import { FaAlignRight } from "react-icons/fa"
+import links from "../constants/links"
+import socialIcons from "../constants/social-icons"
 
 const Footer = () => {
   return (
-    <div>
-      <h1>Footer</h1>
-    </div>
+    <footer className={styles.footer}>
+      <div className={styles.links}>
+        {links.map((item, index) => {
+          return (
+            <Link key={index} to={item.path}>
+              {item.text}
+            </Link>
+          )
+        })}
+      </div>
+      <div className={styles.icons}>
+        {socialIcons.map((item, index) => {
+          return (
+            <a
+              key={index}
+              href={item.url}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {item.icon}
+            </a>
+          )
+        })}
+      </div>
+      <div className={styles.copyright}>
+        copyright &copy; backroads travel company {new Date().getFullYear()}
+        all rights reserved
+      </div>
+    </footer>
   )
 }
 
