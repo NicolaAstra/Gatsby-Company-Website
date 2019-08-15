7605db30dcdbcb8669771e2ed9f7ec021a86c9e5
NicolaAstra <mcsnow78@gmail.com>
2019-08-15 11:32:53 +0700

    styling about commponent

--- src/components/Home/About.js
+++ src/components/Home/About.js
@@ -1,12 +1,36 @@
 import React from "react"
 import Title from "../Title"
+import styles from "../../css/about.module.css"
+
+import images from "../../images/defaultBcg.jpeg"
 
 const About = () => {
+  console.log(images)
   return (
-    <div>
-      About
+    <section className={styles.about}>
       <Title title="about" subtitle="us" />
-    </div>
+      <div className={styles.aboutCenter}>
+        <article className={styles.aboutImg}>
+          <div className={styles.imgContainer}>
+            <img src={images} alt="about company" />
+          </div>
+        </article>
+        <article className={styles.aboutInfo}>
+          <h4>explore the difference</h4>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas natus
+            beatae temporibus.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas natus
+            beatae temporibus.
+          </p>
+          <button type="button" className="btn-primary">
+            read more
+          </button>
+        </article>
+      </div>
+    </section>
   )
 }
 
