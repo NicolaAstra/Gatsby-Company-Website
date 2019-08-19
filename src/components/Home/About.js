0c01d05f5302e6b17db27ee39aa9f81c0f951f94
NicolaAstra <mcsnow78@gmail.com>
2019-08-19 14:45:55 +0700

    fix about image

--- src/components/Home/About.js
+++ src/components/Home/About.js
@@ -1,17 +1,34 @@
 import React from "react"
 import Title from "../Title"
 import styles from "../../css/about.module.css"
+import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
 
-import images from "../../images/defaultBcg.jpeg"
+const getAbout = graphql`
+  query aboutImage {
+    aboutImage: file(relativePath: { eq: "defaultBcg.jpeg" }) {
+      childImageSharp {
+        fluid(maxWidth: 600) {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+  }
+`
 
 const About = () => {
+  const { aboutImage } = useStaticQuery(getAbout)
   return (
     <section className={styles.about}>
       <Title title="about" subtitle="us" />
       <div className={styles.aboutCenter}>
         <article className={styles.aboutImg}>
           <div className={styles.imgContainer}>
-            <img src={images} alt="about company" />
+            {/* <img src={images} alt="about company" /> */}
+            <Img
+              fluid={aboutImage.childImageSharp.fluid}
+              alt="awesome landscape"
+            />
           </div>
         </article>
         <article className={styles.aboutInfo}>
