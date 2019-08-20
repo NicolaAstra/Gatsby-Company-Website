1a1a3b1435006fe600a9d36338e679eb78c1b24d
NicolaAstra <mcsnow78@gmail.com>
2019-08-20 10:33:14 +0700

    setup all page background

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,18 +1,27 @@
 import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/Layout"
+import StyledHero from "../components/StyledHero"
+import { graphql } from "gatsby"
 
-
-const blog = () => {
+const blog = ({ data }) => {
   return (
     <Layout>
-      Blog page <Link to="/"> Back Home</Link>
-   
+      <StyledHero img={data.blogBcg.childImageSharp.fluid} />
     </Layout>
   )
 }
 
-
-
+export const query = graphql`
+  query {
+    blogBcg: file(relativePath: { eq: "blogBcg.jpeg" }) {
+      childImageSharp {
+        fluid(quality: 90, maxWidth: 4160) {
+          ...GatsbyImageSharpFluid_withWebp
+        }
+      }
+    }
+  }
+`
 
 export default blog
