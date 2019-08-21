319326e094afd985e8e245d08f7cfddc901fa502
NicolaAstra <mcsnow78@gmail.com>
2019-08-21 11:08:50 +0700

    add label in contact component

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -10,6 +10,7 @@ const Contact = () => {
         <div className={styles.center}>
           <form className={styles.form}>
             <div>
+              <label htmlFor="name">Name</label>
               <input
                 type="text"
                 name="name"
@@ -19,6 +20,7 @@ const Contact = () => {
               />
             </div>
             <div>
+              <label htmlFor="email">Email</label>
               <input
                 type="email"
                 name="email"
@@ -28,6 +30,7 @@ const Contact = () => {
               />
             </div>
             <div>
+              <label htmlFor="message">Message</label>
               <textarea
                 name="message"
                 id="message"
