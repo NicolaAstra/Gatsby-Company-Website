ad1940fc6d7140e06383a1ef941b982c732b8a90
NicolaAstra <mcsnow78@gmail.com>
2019-08-25 22:34:39 +0700

    finish tours component

---
+++ src/components/Tours/Tours.js
@@ -0,0 +1,32 @@
+import React from "react"
+import TourList from "./TourList"
+import { useStaticQuery, graphql } from "gatsby"
+
+const getTours = graphql`
+  query {
+    tours: allContentfulTour {
+      edges {
+        node {
+          name
+          price
+          slug
+          country
+          contentful_id
+          days
+          images {
+            fluid {
+              ...GatsbyContentfulFluid_tracedSVG
+            }
+          }
+        }
+      }
+    }
+  }
+`
+
+const Tours = () => {
+  const { tours } = useStaticQuery(getTours)
+  return <TourList tours={tours} />
+}
+
+export default Tours
--- src/pages/tours.js
+++ src/pages/tours.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react"
 import Layout from "../components/Layout"
 import StyledHero from "../components/StyledHero"
+import Tours from "../components/Tours/Tours"
 import { graphql } from "gatsby"
 
 export default class tours extends Component {
@@ -8,6 +9,7 @@ export default class tours extends Component {
     return (
       <Layout>
         <StyledHero img={this.props.data.defaultBcg.childImageSharp.fluid} />
+        <Tours />
       </Layout>
     )
   }
