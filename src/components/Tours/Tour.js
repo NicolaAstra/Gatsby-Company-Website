e73a4ed098ee8b9aa217e6ff360d3d8aa1a6e63f
NicolaAstra <mcsnow78@gmail.com>
2019-08-26 11:32:22 +0700

    add propTypes

--- src/components/Tours/Tour.js
+++ src/components/Tours/Tour.js
@@ -4,6 +4,8 @@ import styles from "../../css/tour.module.css"
 import { FaMap } from "react-icons/fa"
 import AniLink from "gatsby-plugin-transition-link/AniLink"
 
+import PropTypes from "prop-types"
+
 const Tour = ({ tour }) => {
   const { name, price, country, days, slug, images } = tour
 
@@ -22,7 +24,7 @@ const Tour = ({ tour }) => {
         <div className={styles.info}>
           <h4 className={styles.country}>
             <FaMap className={styles.icon} />
-            {country}
+            {country || "default country"}
           </h4>
           <div className={styles.details}>
             <h6>{days} days</h6>
@@ -32,6 +34,16 @@ const Tour = ({ tour }) => {
       </div>
     </article>
   )
+}
+
+Tour.propTypes = {
+  tour: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    country: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+    days: PropTypes.number.isRequired,
+    images: PropTypes.arrayOf(PropTypes.object).isRequired,
+  }),
 }
 
 export default Tour
