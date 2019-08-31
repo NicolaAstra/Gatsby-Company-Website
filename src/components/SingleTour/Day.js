8096cf57c2f5608da69dad4bb8b4c48d065a6a8c
NicolaAstra <mcsnow78@gmail.com>
2019-09-01 03:30:20 +0700

    finish single tour compponent

--- src/components/SingleTour/Day.js
+++ src/components/SingleTour/Day.js
@@ -1,7 +1,24 @@
-import React from "react"
+import React, { useState } from "react"
+import { FaAngleDown } from "react-icons/fa"
+import styles from "../../css/day.module.css"
 
-const Day = () => {
-  return <div>Days Components</div>
+const Day = ({ day, info }) => {
+  const [showInfo, setInfo] = useState(false)
+  const toggleInfo = () => {
+    setInfo(showInfo => !showInfo)
+  }
+
+  return (
+    <article className={styles.day}>
+      <h4>
+        {day}
+        <button className={styles.btn} onClick={toggleInfo}>
+          <FaAngleDown />
+        </button>
+      </h4>
+      {showInfo && <p>{info}</p>}
+    </article>
+  )
 }
 
 export default Day
