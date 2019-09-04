d8a47b95241306a77f0ed0f1b065497e633ec349
NicolaAstra <mcsnow78@gmail.com>
2019-09-04 12:47:52 +0700

    setup blogs template

---
+++ src/templates/blog-list-template.js
@@ -0,0 +1,13 @@
+import React from "react"
+import { graphql } from "gatsby"
+import Layout from "../components/Layout"
+import AniLink from "gatsby-plugin-transition-link/AniLink"
+import styles from "../css/blog.module.css"
+import BLogCard from "../components/Blog/BlogCard"
+import Title from "../components/Title"
+
+const Blog = () => {
+  return <div>Blog list template component</div>
+}
+
+export default Blog
