6c0af63497f981aff7b48b5f51db5c4c45300d43
NicolaAstra <mcsnow78@gmail.com>
2019-09-05 09:19:20 +0700

    setup blogs card template

--- src/templates/blog-list-template.js
+++ src/templates/blog-list-template.js
@@ -3,11 +3,46 @@ import { graphql } from "gatsby"
 import Layout from "../components/Layout"
 import AniLink from "gatsby-plugin-transition-link/AniLink"
 import styles from "../css/blog.module.css"
-import BLogCard from "../components/Blog/BlogCard"
+import BlogCard from "../components/Blog/BlogCard"
 import Title from "../components/Title"
 
-const Blog = () => {
-  return <div>Blog list template component</div>
+const Blog = props => {
+  const { data } = props
+
+  return (
+    <section className={styles.blog}>
+      <Title title="latest" subtitle="posts" />
+      <div className={styles.center}>
+        {data.posts.edges.map(({ node }) => {
+          return <BlogCard key={node.id} blog={node} />
+        })}
+      </div>
+    </section>
+  )
 }
+
+export const query = graphql`
+  query getPosts($skip: Int!, $limit: Int!) {
+    posts: allContentfulPost(
+      skip: $skip
+      limit: $limit
+      sort: { fields: published, order: DESC }
+    ) {
+      edges {
+        node {
+          slug
+          title
+          id: contentful_id
+          published(formatString: "MMMM Do, YYYY")
+          image {
+            fluid {
+              ...GatsbyContentfulFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`
 
 export default Blog
